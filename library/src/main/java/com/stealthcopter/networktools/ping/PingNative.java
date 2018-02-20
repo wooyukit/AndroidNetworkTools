4d1b5896ff3fe967fef17a12d854a1346b61d170
WOO Yu Kit Vincent <wooyukit@gmail.com>
2018-02-20 16:49:00 +0800

    ADDED: log message to debug

--- library/src/main/java/com/stealthcopter/networktools/ping/PingNative.java
+++ library/src/main/java/com/stealthcopter/networktools/ping/PingNative.java
@@ -45,13 +45,29 @@ public class PingNative {
 
         Process proc = runtime.exec(pingCommand + " -c 1 -W " + timeoutSeconds + " " + address);
         proc.waitFor();
-        InputStreamReader reader = new InputStreamReader(proc.getInputStream());
-        BufferedReader buffer = new BufferedReader(reader);
-        String line;
-        while ((line = buffer.readLine()) != null) {
-            echo.append(line).append("\n");
+        int exit = proc.exitValue();
+        String pingError;
+        System.out.print("Exit Code: " + exit);
+        Log.w("AndroidNetworkTools", "Exit Code: " + exit);
+        if (exit == 0) {
+            InputStreamReader reader = new InputStreamReader(proc.getInputStream());
+            BufferedReader buffer = new BufferedReader(reader);
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                echo.append(line).append("\n");
+            }
+            return getPingStats(pingResult, echo.toString());
+        } else {
+            InputStreamReader reader = new InputStreamReader(proc.getErrorStream());
+            BufferedReader buffer = new BufferedReader(reader);
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                echo.append(line).append("\n");
+            }
+            pingError = "error = " + echo + ", exit = " + exit;
         }
-        return getPingStats(pingResult, echo.toString());
+        pingResult.error = pingError;
+        return pingResult;
     }
 
     /**
